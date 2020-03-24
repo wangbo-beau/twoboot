f8d3f76269bb5b9de06da7bf0d8a31a228cbe14f
wangbo-lenovo <wangbo_dream@163.com>
2020-03-24 14:35:24 +0800

    创建twoboot

---
+++ src/main/java/com/xjtu/twoboot/TwobootApplication.java
@@ -0,0 +1,13 @@
+package com.xjtu.twoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwobootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwobootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xjtu/twoboot/TwobootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjtu.twoboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwobootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
